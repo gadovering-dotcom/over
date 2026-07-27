dfcd5834fd1e19f2bb69a68903b5eb5b31836e07
gadovering-dotcom <gadovering@gmail.com>
2026-07-28 01:18:52 +0330

    Connect authentication API

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from backend.database import init_db
 from backend.routines import router as routines_router
+from backend.auth import router as auth_router
 
 app = FastAPI(title="Over Daily Routine")
 
@@ -9,6 +10,7 @@ def startup():
     init_db()
 
 app.include_router(routines_router)
+app.include_router(auth_router)
 
 @app.get("/")
 def home():
