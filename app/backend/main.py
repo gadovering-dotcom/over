3fbb30a442a7e7f92344759c5e853711ac63eb84
gadovering-dotcom <gadovering@gmail.com>
2026-07-28 01:15:30 +0330

    Connect routine API to backend

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from backend.database import init_db
+from backend.routines import router as routines_router
 
 app = FastAPI(title="Over Daily Routine")
 
 @app.on_event("startup")
 def startup():
     init_db()
+
+app.include_router(routines_router)
 
 @app.get("/")
 def home():
